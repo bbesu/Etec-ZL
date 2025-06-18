0d74e47836ed3f1acb02d42ca71dec4e5319bca0
Besu <bbesu1606@gmail.com>
2025-06-18 19:56:13 -0300

    Add files via upload

---
+++ PW/JavaScript3B/JavaScriptAtv/JavaScript/script.js
@@ -0,0 +1,31 @@
+/*window.alert("Meu segundo alerta")
+window.confirm("Aceita?")*/
+/*
+
+let num1 ==Number(window.prompt("Digite um numero"))
+let num2 == Number(window.prompt("Digite um segundo numero"))
+window.alert(num1)
+window.alert(num2)
+let soma == num1 + num2
+window.alert(soma)*/
+
+/* numero + string, mas não string + string == /* num1num2*/
+
+/*
+var nome== window.prompt("Coloque seu nome")
+let sobrenome == window.prompt("Coloque seu sobrenome")
+console.log("Seja bem vindo " + nome + " " + sobrenome)
+console.log(`Seja bem vindo ${nome}  ${sobrenome}`)
+*/
+
+let num1 = Number(window.prompt("Coloque o primeiro numero"))
+let num2 = Number(window.prompt("Coloque o segundo numero"))
+let alg = (window.prompt("Coloque + - / *"))
+if(alg=="+"){
+console.log(`o resultado de ${num1} + ${num2} e ` + (num1+num2))}
+else if(alg=="-"){
+    console.log(`o resultado de ${num1} - ${num2} e ` + (num1-num2))}
+    else if(alg=="*"){
+        console.log(`o resultado de ${num1} * ${num2} e ` + (num1*num2))}
+        else if(alg=="/"){
+            console.log(`o resultado de ${num1} / ${num2} e ` + (num1/num2))}
